50193bb6fdd9e844fc58c2853abaac544e677932
Matt Trussell <smtrussell@gmail.com>
2018-09-02 18:15:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902175515) do
-
-  create_table "coupons", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
